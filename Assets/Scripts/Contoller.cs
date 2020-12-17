e326e5b624151277eeffa715fa350a11ae2d4caf
unknown <vorozheykin1997@bk.ru>
2020-12-17 19:46:49 +0400

    new features: shooting, advanced moving

--- Assets/Scripts/Contoller.cs
+++ Assets/Scripts/Contoller.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class Contoller : MonoBehaviour
 {
+    //Передвижение
     public float horizontalSpeed;
-    float speedX;
     public float verticalImpulse;
+    float speedX;
     Rigidbody2D rb;
+        // Прыжки
     bool isGrounded;
-
+        // Развороты
+    bool isTurned = false;
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -20,11 +23,18 @@ public class Contoller : MonoBehaviour
         if (Input.GetKey(KeyCode.A))
         {
             speedX = -horizontalSpeed;
-
+            if(isTurned == false)
+            {
+                Turning();
+            }
         }
         else if (Input.GetKey(KeyCode.D))
         {
             speedX = horizontalSpeed;
+            if (isTurned == true)
+            {
+                Turning();
+            }
         }
         transform.Translate(speedX, 0, 0);
         speedX = 0;
@@ -32,12 +42,11 @@ public class Contoller : MonoBehaviour
 
     public void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        if (Input.GetKeyDown(KeyCode.W) && isGrounded)
         {
             rb.AddForce(new Vector2(0, verticalImpulse), ForceMode2D.Impulse);
         }
     }
-
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Ground")
@@ -50,5 +59,11 @@ public class Contoller : MonoBehaviour
             isGrounded = false;
     }
 
-
+    private void Turning()
+    {
+        isTurned = !isTurned;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+    }
 }
