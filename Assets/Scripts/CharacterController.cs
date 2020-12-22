87c7b7e81c29e186d57c41b6524b50cd5d2cc2d8
bonnysid <bonnysidworker@gmail.com>
2020-12-23 01:53:01 +0400

    add to character run,jump,idle

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public float maxSpeed = 10f;
+    private bool isFacingRight = true;
+    private Animator anim;
+    private Rigidbody2D rigidbody2D;
+    private bool isGrounded = false;
+    public Transform groundCheck;
+    private float groundRadius = 0.2f;
+    public LayerMask whatIsGround;
+
+    private void Start() {
+        anim = GetComponent<Animator>();
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate() {
+
+        float move = Input.GetAxis("Horizontal");
+
+        anim.SetFloat("Speed", Mathf.Abs(move));
+
+        rigidbody2D.velocity = new Vector2(move * maxSpeed, rigidbody2D.velocity.y);
+
+        if (move > 0 && !isFacingRight) Flip();
+        else if (move < 0 && isFacingRight) Flip();
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+        anim.SetBool("Ground", isGrounded);
+        anim.SetFloat("vSpeed", rigidbody2D.velocity.y);
+
+        if (!isGrounded) return;
+    }
+
+    private void Update() {
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space)) {
+            anim.SetBool("Ground", false);
+            rigidbody2D.AddForce(new Vector2(0, 600));
+        }
+    }
+
+
+    private void Flip() {
+        isFacingRight = !isFacingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
